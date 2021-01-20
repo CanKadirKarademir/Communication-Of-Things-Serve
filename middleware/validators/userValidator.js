a9b341f4deb9ebc8fc351719713be25f1a5d72b7
Kadir Can KARADEMİR <50851192+CanKadirKarademir@users.noreply.github.com>
2021-01-20 18:21:19 +0300

    refactor: refactor user router and validator

--- middleware/validators/userValidator.js
+++ middleware/validators/userValidator.js
@@ -1,7 +1,57 @@
+const joi = require("joi");
+const HttpStatusCode = require("http-status-codes");
 const CommonValidator = require("./commonValidator");
 
 class UserValidator extends CommonValidator {
   constructor() {}
+
+  static async update(req, res, next) {
+    try {
+      await joi
+        .object({
+          Id: joi.number().required(),
+          UserFirstName: joi
+            .string()
+            .max(50)
+            .pattern(new RegExp("^[A-Za-zÇçÖöŞşÜüĞğİı ]+$")),
+          UserLastName: joi
+            .string()
+            .max(50)
+            .pattern(new RegExp("^[A-Za-zÇçÖöŞşÜüĞğİı ]+$")),
+          UserEmail: joi.string().max(50).email(),
+          UserPassword: joi.string().max(50).required(),
+        })
+        .validateAsync(req.body);
+      next();
+    } catch (err) {
+      res.status(HttpStatusCode.EXPECTATION_FAILED).send(err.message);
+    }
+  }
+
+  static async insert(req, res, next) {
+    try {
+      await joi
+        .object({
+          UserFirstName: joi
+            .string()
+            .max(50)
+            .pattern(new RegExp("^[A-Za-zÇçÖöŞşÜüĞğİı ]+$"))
+            .required(),
+          UserLastName: joi
+            .string()
+            .max(50)
+            .pattern(new RegExp("^[A-Za-zÇçÖöŞşÜüĞğİı ]+$"))
+            .required(),
+          UserEmail: joi.string().max(50).email().required(),
+
+          UserPassword: joi.string().max(50).required(),
+        })
+        .validateAsync(req.body);
+      next();
+    } catch (err) {
+      res.status(HttpStatusCode.EXPECTATION_FAILED).send(err.message);
+    }
+  }
 }
 
 module.exports = UserValidator;
--- routers/userRouter.js
+++ routers/userRouter.js
@@ -8,6 +8,22 @@ const HttpStatusCode = require("http-status-codes");
 const { errorSender } = require("../utils");
 
 router.get(
+  "/user",
+  tokenControl,
+  userValidator.limitAndOffset,
+  async (req, res) => {
+    try {
+      const result = await userTransactions.selectAsync(req.query);
+      res.json(result);
+    } catch (err) {
+      res
+        .status(err.status || HttpStatusCode.INTERNAL_SERVER_ERROR)
+        .send(err.message);
+    }
+  }
+);
+
+router.get(
   "/user/:Id",
   tokenControl,
   userValidator.paramId,
@@ -22,5 +38,60 @@ router.get(
     }
   }
 );
+
+router.delete("/user", tokenControl, userValidator.bodyId, async (req, res) => {
+  try {
+    const result = await userTransactions.deleteAsync(req.body);
+    if (!result.affectedRows)
+      throw errorSender.errorObject(
+        HttpStatusCode.GONE,
+        "There is no such user ID in the system !"
+      );
+    res.json("The user registration was deleted successfully.");
+  } catch (err) {
+    res
+      .status(err.status || HttpStatusCode.INTERNAL_SERVER_ERROR)
+      .send(err.message);
+  }
+});
+
+router.put("/user", tokenControl, userValidator.update, async (req, res) => {
+  try {
+    const result = await userTransactions.updateAsync(req.body, {
+      Id: req.body.UserID,
+    });
+    if (!result.affectedRows)
+      throw errorSender.errorObject(
+        HttpStatusCode.GONE,
+        "There is no such user ID in the system !"
+      );
+    res.json("User information has been updated");
+  } catch (err) {
+    res
+      .status(err.status || HttpStatusCode.INTERNAL_SERVER_ERROR)
+      .send(err.message);
+  }
+});
+
+router.post("/user", tokenControl, userValidator.insert, async (req, res) => {
+  try {
+    const result = await userTransactions.insertAsync(req.body);
+    if (!result.affectedRows)
+      throw errorSender.errorObject(
+        HttpStatusCode.INTERNAL_SERVER_ERROROR,
+        "There was a problem adding the user !"
+      );
+    res.json("User registered.");
+  } catch (err) {
+    if (err.errno === 1062)
+      res
+        .status(HttpStatusCode.CONFLICT)
+        .send("Email address is already registered in the system !");
+    else
+      res
+        .status(err.status || HttpStatusCode.INTERNAL_SERVER_ERROR)
+        .send(err.message);
+  }
+});
 
 module.exports = router;
